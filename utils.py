c6796ec56e80659ec953ea9d7344d7894385f919
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-09-20 14:34:26 -0300

    fix: move validation functions out of data processing function

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from exceptions import NegativeTitleError, InvalidYearCupError, ImpossibleTitlesError
+from exceptions import NegativeTitlesError, InvalidYearCupError, ImpossibleTitlesError
 from datetime import datetime
 
 
@@ -8,33 +8,30 @@ def data_processing(team: dict):
     first_cup_str = team['first_cup']
     first_cup_date = datetime.strptime(first_cup_str, "%Y-%m-%d")
     first_cup_year = first_cup_date.year
+    last_cup_year = 2022
 
     now = datetime.now()
     possible_cups = (int(now.year) - int(first_cup_year)) / 4
 
-    def validate_titles(titles: int):
+    validate_titles(team_titles)
+    validate_cup_year(first_cup_year)
+    validate_titles_number(team_titles, possible_cups, first_cup_year, last_cup_year)
+
+
+def validate_titles(titles: int):
         if titles < 0:
-            raise NegativeTitleError()
+            raise NegativeTitlesError("titles cannot be negative")
 
-    try:
-        validate_titles(team_titles)
-    except NegativeTitleError as err:
-        return err.message
 
-    def validate_cup_year(year: int):
+def validate_cup_year(year: int):
         if (year - 1930) % 4 != 0 or year < 1930:
-            raise InvalidYearCupError()
+            raise InvalidYearCupError("there was no world cup this year")
+        
 
-    try:
-        validate_cup_year(first_cup_year)
-    except InvalidYearCupError as err:
-        return err.message
+def validate_titles_number(titles: int, possible_cups: int, first_cup_year:int, last_cup_year: int):    
+    if titles > possible_cups:
+        if first_cup_year == last_cup_year and titles == 1:
+            pass
+        else:
+            raise ImpossibleTitlesError("impossible to have more titles than disputed cups")
 
-    def validate_titles_number(titles: int):
-        if titles > possible_cups:
-            raise ImpossibleTitlesError()
-
-    try:
-        validate_titles_number(team_titles)
-    except ImpossibleTitlesError as err:
-        return err.message
